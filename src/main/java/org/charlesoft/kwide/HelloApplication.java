337cee5f5d246f26309a50173e114fefafd5be71
kerware <olivier.charles@kerware.com>
2024-03-26 19:17:55 +0100

    Premier commit

--- src/main/java/org/charlesoft/kwide/HelloApplication.java
+++ src/main/java/org/charlesoft/kwide/HelloApplication.java
@@ -11,9 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Scene scene = new Scene( 320, 240);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+
         stage.show();
     }
 
